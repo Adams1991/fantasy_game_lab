99849af607c2670eb9113e5997e6a0e06ed65802
helenosheaa <helenosheaa@gmail.com>
2018-07-04 10:56:22 +0100

    setup classes

---
+++ src/test/java/Game/Characters/WizardTest.java
@@ -0,0 +1,12 @@
+package Game.Characters;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class WizardTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
