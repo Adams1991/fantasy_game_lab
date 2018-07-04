0996b3e9d0095b71f7d3b3227398cd101e48165f
helenosheaa <helenosheaa@gmail.com>
2018-07-04 14:57:22 +0100

    can get first room

---
+++ src/main/java/Game/Game.java
@@ -0,0 +1,40 @@
+package Game;
+
+import Game.Characters.Dwarf;
+import Game.Rooms.JungleRoom;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Room> rooms;
+    private ArrayList<GameCharacter> characters;
+
+    public Game(ArrayList<Room> rooms, ArrayList<GameCharacter> characters){
+        this.rooms = rooms;
+        this.characters = characters;
+    }
+
+
+    public int getCharacterArraySize() {
+        return this.characters.size();
+    }
+
+    public void addCharacter(GameCharacter character) {
+        this.characters.add(character);
+    }
+
+    public void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+
+    public int getRoomArraySize() {
+        return this.rooms.size();
+    }
+
+
+    public Room getFirstRoom() {
+        return this.rooms.get(0);
+    }
+}
---
+++ src/test/java/Game/GameTest.java
@@ -0,0 +1,58 @@
+package Game;
+
+import Game.Characters.Cleric;
+import Game.Characters.Dwarf;
+import Game.Characters.Enemy;
+import Game.Rooms.JungleRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    Game game;
+    Treasure treasure;
+    Enemy enemy;
+    JungleRoom room;
+    ArrayList<Room> rooms;
+    ArrayList<GameCharacter> characters;
+    Dwarf dwarf;
+
+    @Before
+    public void before() {
+        dwarf = new Dwarf(50, 100);
+        enemy = new Enemy(50, "Troll", 100);
+        treasure = new Treasure("Gem", 100);
+        room = new JungleRoom(treasure, enemy);
+        rooms = new ArrayList<>();
+        characters = new ArrayList<>();
+        game = new Game(rooms, characters);
+    }
+
+    @Test
+    public void canAddCharacter(){
+        game.addCharacter(dwarf);
+        assertEquals(1,  game.getCharacterArraySize());
+    }
+
+    @Test
+    public void canAddRoom(){
+        game.addRoom(room);
+        assertEquals(1,  game.getRoomArraySize());
+    }
+
+    @Test
+    public void canGetFirstRoom(){
+        game.addRoom(room);
+        game.addCharacter(dwarf);
+        assertEquals(room, game.getFirstRoom());
+    }
+
+//    @Test
+//    public void canCompleteRoom(){
+//        assertEquals();
+//    }
+}
